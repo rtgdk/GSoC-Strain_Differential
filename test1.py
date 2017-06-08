48c553267c0a2d531c291bb71628bbc89e063ec6
ravaan <arpit.agarwal181@gmail.com>
2017-06-09 01:59:31 +0530

    minor change

--- test1.py
+++ test1.py
@@ -51,5 +51,5 @@ count = 0
 
 for i in REF_list:
     for record in vcf_reader.fetch(REF_list[0],REF_list[int(i)] - 1, REF_list[int(i)]):
-        print record.REF , record.POS - REF_list[i]
+        print record.REF , record.POS - REF_list[int(i)]
 
