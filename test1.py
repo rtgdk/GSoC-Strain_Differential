51864184e4d19877c656e5b27456ee8155b87cbd
ravaan <arpit.agarwal181@gmail.com>
2017-06-11 02:11:38 +0530

    writting test minor change

--- test1.py
+++ test1.py
@@ -7,29 +7,25 @@ vcf_reader = vcf.Reader(open('/home/ubuntu/GSoC-Strain_Diffrential/test.vcf.gz')
 snp_count = 0
 indel_count = 0
 for record in vcf_reader:
-    # if record.is_snp:
-    # 	snp_count = snp_count + 1
-    # 	print "\nSNP Found :"
-    # 	print "Record Position: ", record.POS, "\nRecord REF: ", record.REF, "\nRecord ALT:", record.ALT
+    if record.is_snp:
+    	snp_count = snp_count + 1
+    	print "\nSNP Found :"
+    	print "Record Position: ", record.POS, "\nRecord REF: ", record.REF, "\nRecord ALT:", record.ALT
     if record.is_indel:
         indel_count = indel_count + 1
         print "\nINDEL Found:"
         print "Record Position: ", record.POS, "\nRecord REF: ", record.REF, "\nRecord ALT:", record.ALT, "\nDeletion: ", record.is_deletion
-    if indel_count > 5:
+    if indel_count > 5 || snp_count > 30 :
         break
 print "\nIndel Count: ", indel_count, "\nSnp count: ", snp_count
 vcf_writer = vcf.Writer(open('/dev/null', 'w'), vcf_reader)
 # vcf_writer = vcf.Writer(open('NUL', 'w'), vcf_reader)
 
-count = 0
+count = 1
 record = next(vcf_reader)
 REF_list = [record.CHROM]
 for record in vcf_reader:
     print record.REF, record.POS
-    # if record.REF != 'C':
-    #     record.REF = 'C'
-    # else:
-    #     record.REF = 'A'
     record.REF = 'C'
     print record.REF
     REF_list.extend([record.POS])
@@ -40,16 +36,8 @@ for record in vcf_reader:
 
 
 print("REF_list", REF_list)
-count = 0
-# print "\n\n Checking\n"
-# for i in range(0, len(REF_list)):
-#     for record in vcf_reader.fetch(REF_list[0], i-1, i):
-#         print record.REF, record.POS
-#         count = count + 1
-#         if count > 100:
-#             break
 
 for i in REF_list:
-    for record in vcf_reader.fetch(REF_list[0],int(i) - 1, int(i)):
+    for record in vcf_reader.fetch(REF_list[0],int(i), int(i) + 1):
         print record.REF , record.POS - int(i)
 
