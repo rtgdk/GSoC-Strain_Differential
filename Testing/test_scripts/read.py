94391c85e92868b35af3f67770b38b0432df2313
ravaan <arpit.agarwal181@gmail.com>
2017-06-26 14:28:09 +0000

    structure changes

--- Testing/test_scripts/read.py
+++
@@ -1,5 +0,0 @@
-import vcf
-
-vcf_reader = vcf.Reader(open('C:\\Users\\arpit\\Documents\\GitHub\\GSoC-Strain_Diffrential\\test.vcf', 'r'))
-for record in vcf_reader:
-	print record.CHROM, record.POS, record.REF, record.ALT
--- Testing/test_scripts/write_test.py
+++ Testing/test_scripts/write_test.py
@@ -1,34 +1,9 @@
 import vcf
 
-
-def writer():
-    data_file_path = "/home/ubuntu/GSoC-Strain_Diffrential/test.vcf.gz"
-    print "Enter the data file path"
-    temp_path = raw_input("type d for default path else type a path and file name:")
-    if temp_path != "d":
-        data_file_path = temp_path
-
-    vcf_reader = vcf.Reader(open(data_file_path))
-
-    write_file_path = "/home/ubuntu/GSoC-Strain_Diffrential/test_modified.vcf"
-    print "Enter writing path and file name"
-    temp_path = raw_input("type d for default path else type a path and file name:")
-    if temp_path != "d":
-        write_file_path = temp_path
-
-    vcf_writer = vcf.Writer(open(write_file_path, 'w'), vcf_reader)
-
-    # Storing the chrom no. of the data - as only 100 bases are read, the chrom no. will not change only one is saved.
-    count = 1
-    record = next(vcf_reader)
-    ref_list = [record.CHROM]
-    for record in vcf_reader:
-        record.REF = 'C'
-        print record.REF
-        ref_list.extend([record.POS])
-        vcf_writer.write_record(record)
-        vcf_writer.flush()
-        count = count + 1
-        if count > 10:
-            break
-
+data_path = "/home/ubuntu/GSoC-Strain_Diffrential/original_modified.vcf.gz"
+temp_path= raw_input("enter file path, d for default path:")
+if temp_path != "d":
+	data_path = temp_path
+vcf_reader = vcf.Reader(open(data_path,'r'))
+for record in vcf_reader:
+	print record.CHROM, record.POS, record.REF, record.ALT
