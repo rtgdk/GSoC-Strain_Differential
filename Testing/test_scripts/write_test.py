87046885f8a7fa9e5cfc1f08324f84f8b61f4e1a
ravaan <arpit.agarwal181@gmail.com>
2017-06-26 20:07:34 +0530

    added pytest for write.py

--- Testing/test_scripts/write_test.py
+++ Testing/test_scripts/write_test.py
@@ -1,9 +1,18 @@
 import vcf
 
-data_path = "/home/ubuntu/GSoC-Strain_Diffrential/original_modified.vcf.gz"
-temp_path= raw_input("enter file path, d for default path:")
-if temp_path != "d":
-	data_path = temp_path
-vcf_reader = vcf.Reader(open(data_path,'r'))
-for record in vcf_reader:
-	print record.CHROM, record.POS, record.REF, record.ALT
+
+def readfile():
+    data_path = "/home/ubuntu/GSoC-Strain_Diffrential/original_modified.vcf.gz"
+    temp_path = raw_input("enter file path, d for default path:")
+    if temp_path != "d":
+        data_path = temp_path
+    vcf_reader = vcf.Reader(open(data_path, 'r'))
+    # for record in vcf_reader:
+    #     print record.CHROM, record.POS, record.REF, record.ALT
+    return vcf_reader
+
+
+def testfunc():
+    vcf_reader = readfile()
+    for record in vcf_reader:
+        assert record.REF == "C"
